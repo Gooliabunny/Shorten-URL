ea8db618df7ce67f755565b0109fc6bda9b4caa4
TABRIZ NABIZADE <tabriz.nabizadeh@yahoo.com>
2026-01-22 02:27:54 +0400

    Add frontend and backend tests

---
+++ node_modules/backend/jest.config.js
@@ -0,0 +1,12 @@
+export default {
+    testEnvironment: 'node',
+    transform: {},
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    testMatch: ['**/*.test.js'],
+    collectCoverageFrom: [
+        'server.js',
+        '!node_modules/**',
+    ],
+};
